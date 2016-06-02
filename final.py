3d05e17f6ea75da239bea2037673e45fdb3035ce
Yash Mehra <ymehra@calpoly.edu>
2016-06-02 11:03:30 -0700

    Added reader

---
+++ final.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn
+seaborn.set()
+from numpy import genfromtxt
+
+
+data = genfromtxt('result.csv', delimiter=',')
