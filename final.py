43b8c90acf593755ca065cb0b644ba2440721a40
Yash Mehra <ymehra@calpoly.edu>
2016-06-02 14:29:32 -0700

    Added reader

--- final.py
+++ final.py
@@ -9,7 +9,39 @@ def get_data(filename):
 
     return genfromtxt(filename, delimiter=',')
 
+def get_gr():
+    gr = []
+    for star in data[1:]:
+        g = star[4]
+        r = star[5]
+        color = g - r
+        gr.append(color)
+    return gr
 
-data = get_data('result.csv')
+def get_id():
+    id = []
+    for star in data[1:]:
+        id.append(star[0])
+    return id
+
+##########################################################################################
+#Call functions here
+
+data = get_data('stars.csv')
+#Data format
+#objid,ra,dec,u,g,r,i,z,distance
+#id = 0
+#ra = 1
+#dec = 2
+#u = 3
+#g = 4
+#r = 5
+#i = 6
+#z = 7
+#distance = 8
 print (data.shape)
-print (data)
+
+gr = get_gr()
+id = get_id()
+print (gr)
+print (id)
