e8fd26f9ee5dfcdd9f5f313958df4e144553a300
shsu03 <shsu03@calpoly.edu>
2016-06-03 12:50:38 -0700

    stuff

--- final.py
+++ final.py
@@ -3,6 +3,7 @@ import sklearn
 from sklearn.cluster import KMeans
 import matplotlib
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import seaborn
 seaborn.set()
 from numpy import genfromtxt
@@ -78,6 +79,14 @@ def plot_hr(bv, lum, dist):
 
 
 
+def plot_dist(ra, dec):
+    plt.scatter(ra, dec, s=15, c='green', marker='*', alpha=0.7)
+    plt.ylabel("Declination")
+    plt.xlabel("Right ascension")
+    plt.show()
+
+
+
 def get_RA():
     return data[:, 2]
 
@@ -87,9 +96,28 @@ def get_DEC():
     return data[:, 3]
 
 
+def lum_kmeans(bv, lum):
+    plot = []
+
+    for s in range(data.shape[0]):
+        plot.append(np.array([bv[s], lum[s]]))
+
+    plot = np.array(plot)
+
+    # literally just took from notebook example, will mess around w params of KMeans later?
+    clustering = KMeans(n_clusters=4, init='k-means++', n_init=5)
+    clustering.fit(plot)
+    clusters = clustering.predict(plot)
+
+    plot_2Dclusters(clusters, bv, lum)
+
+    return clusters
+
+
+
 
 def dist_kmeans(ra, dec):
-    # list of stars'[bv, lum] values
+    # list of stars'[ra, dec] values
     plot = []
 
     for s in range(data.shape[0]):
@@ -98,11 +126,43 @@ def dist_kmeans(ra, dec):
     plot = np.array(plot)
 
     # literally just took from notebook example, will mess around w params of KMeans later?
-    clustering = KMeans(n_clusters=3, init='random', n_init=5)
+    clustering = KMeans(n_clusters=3, init='k-means++', n_init=5)
     clustering.fit(plot)
     clusters = clustering.predict(plot)
 
     return clusters
+
+
+
+def plot_2Dclusters(clusters, x, y):
+    cluster0 = clusters == 0
+    cluster1 = clusters == 1
+    cluster2 = clusters == 2
+
+    plt.scatter(x[cluster0], y[cluster0], marker='D', c='blue')
+    plt.scatter(x[cluster1], y[cluster1], marker='o', c='green')
+    plt.scatter(x[cluster2], y[cluster2], marker='^', c='magenta')
+    plt.xlabel("Right Ascension")
+    plt.ylabel("Declination")
+    plt.show()
+
+
+
+def plot3D(x, y, z, clusters):
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+    cluster0 = clusters == 0
+    cluster1 = clusters == 1
+    cluster2 = clusters == 2
+
+    ax.scatter(x[cluster0], y[cluster0], z[cluster0], marker='D', c='blue')
+    ax.scatter(x[cluster1], y[cluster1], z[cluster1], marker='o', c='green')
+    ax.scatter(x[cluster2], y[cluster2], z[cluster2], marker='^', c='magenta')
+    ax.set_ylabel("Distance")
+    ax.set_xlabel("Declination")
+    ax.set_zlabel("Right Ascension")
+    plt.show()
 
 
 ##########################################################################################
@@ -133,8 +193,10 @@ parallax = get_parallax()
 
 dist = get_dist(parallax)
 # print(dist)
-#plot_hr(bv, lum, dist)
-
-
+plot_hr(bv, lum, dist)
+#
+# plot_dist(ra, dec)
 distClusters = dist_kmeans(ra, dec)
-print(distClusters)
+# print(distClusters)
+# plot_2Dclusters(distClusters, ra, dec)
+plot3D(dec, dist, ra, distClusters)
